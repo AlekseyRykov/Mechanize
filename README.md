c0510a88c82e64ddd6440f3b7764a6912bc83409
Aleksey Rykov <a.rykov8923@gmail.com>
2018-10-15 17:22:59 +1100

    Feature
    update readme.md
    add first version of code

--- README.md
+++ README.md
@@ -6,4 +6,7 @@
 The Mechanize library is used for automating interaction with websites. Mechanize automatically stores and sends cookies, follows redirects, and can follow links and submit forms. Form fields can be populated and submitted. Mechanize also keeps track of the sites that you have visited as a history.
 
 ## Scraping Data
-Mechanize uses nokogiri to parse HTML. What does this mean for you? You can treat a mechanize page like an nokogiri object. After you have used Mechanize to navigate to the page that you need to scrape, then scrape it using nokogiri methods:
+Mechanize uses nokogiri to parse HTML. What does this mean for you? You can treat a mechanize page like an nokogiri object. After you have used Mechanize to navigate to the page that you need to scrape, then scrape it using nokogiri methods.
+
+##
+_This is a training and not self-serving project. Feel free to contact me with questions an suggestions._
---
+++ gamerankings.com/mech_grnks.rb
@@ -0,0 +1,57 @@
+# add require statements.
+require 'rubygems'
+require 'mechanize'
+require 'json'
+
+cert_store = OpenSSL::X509::Store.new
+cert_store.add_file 'cacert.pem'
+
+agent = Mechanize.new
+agent.cert_store = cert_store
+
+page = agent.get("https://www.gamerankings.com/browse.html?site=ps2&cat=0&year=0&numrev=3&sort=2&letter=&search=")
+
+playsation2 = []
+
+next_page = true
+
+while next_page
+
+  g_page = page.links_with(:href => %r{^/ps2/\w+})
+  g_page.map do |link|
+    a = link.click
+    platform = a.search('.crumbs a[1]').text
+    title = a.search('h1[2]').text
+    main_genre = a.search('.crumbs a[2]').text
+    second_genre = a.search('.crumbs a[3]').text
+    third_genre = a.search('.crumbs a[4]').text
+    forth_genre = a.search('.crumbs a[5]').text
+    rank = a.search('span b').text
+
+    playsation2.push(
+      title: title,
+      genre: [main_genre, second_genre, third_genre, forth_genre],
+      platform: platform,
+      rank: rank,
+      )
+  end
+
+  if page.body.include?('Next Page')
+    n_page = page.link_with(:text => 'Next Page').click
+    page = agent.get(n_page)
+  else
+    next_page = false
+  end
+end
+
+puts JSON.pretty_generate(playsation2)
+
+
+
+
+
+
+
+
+
+
